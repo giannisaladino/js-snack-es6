39aaeb4ffe5309b980eee6f5ab398bb0f5d764b5
giannisaladino <saladinogianni6@gmail.com>
2024-03-20 17:25:07 +0100

    SNACK 3 FINISHED

--- js/app.js
+++ js/app.js
@@ -122,11 +122,83 @@ const bici = [
     {
         nome: 'KTM',
         peso: 11,
+    },
+    {
+        nome: 'Bici5',
+        peso: 19,
+    },
+    {
+        nome: 'Bici6',
+        peso: 20,
+    },
+    {
+        nome: 'Bici7',
+        peso: 10,
+    },
+    {
+        nome: 'Bici8',
+        peso: 21,
     }
 ]
 
-for (let i = 0; i < bici.length; i++) {
-    const pesoBici = bici[i].peso;
-    console.log(pesoBici);
-    
+//Dichiaro una variabile che memorizzi l'elemento piu piccolo trovato a partire dal primo elemento (ovvero il primo)
+lightestBike = bici[0];
+
+for (let i = 1; i < bici.length; i++) {
+    const currentElement = bici[i];
+
+    if (currentElement.peso < lightestBike.peso) {
+        lightestBike = currentElement;
+    }
 }
+
+// Stampo in console la bici con peso minore utilizzando destructuring e template literal
+const {nome, peso} = lightestBike;
+console.log(`La bici più leggera è ${nome}, che pesa ${peso}kg`);
+
+
+
+// SNACK 4
+
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+
+const teams = [
+    {
+        nome: 'Juventus',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Milan',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Inter',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Fiorentina',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Atalanta',
+        punti: 0,
+        falli: 0
+    },
+]
+
+// Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
+
+for (let i = 0; i < teams.length; i++) {
+    let puntiFatti = parseInt(Math.random()* 100) + 1;
+    console.log(puntiFatti);
+    let falliSubiti = parseInt(Math.random()* 50) + 1;
+    console.log(falliSubiti);   
+}
+
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
